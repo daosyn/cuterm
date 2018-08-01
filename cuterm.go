a24a4f67a7c80cc292a53d7ced50f1a61f69411c
daosyn <elmobey@gmail.com>
2018-08-01 08:02:40 -0500

    add moves array and mainloop

--- cuterm.go
+++ cuterm.go
@@ -1,6 +1,16 @@
 package main
 
 import "github.com/nsf/termbox-go"
+import "fmt"
+
+var moves = [18]string{
+    "F", "F'", "F2",
+    "R", "R'", "R2",
+    "L", "L'", "L2",
+    "U", "U'", "U2",
+    "D", "D'", "D2",
+    "B", "B'", "B2",
+}
 
 func main() {
     err := termbox.Init()
@@ -8,4 +18,15 @@ func main() {
         panic(err)
     }
     defer termbox.Close()
+    termbox.SetInputMode(termbox.InputEsc)
+mainloop:
+    for {
+        switch ev := termbox.PollEvent(); ev.Type {
+        case termbox.EventKey:
+            if ev.Key == termbox.KeyEsc {
+                break mainloop
+            }
+        }
+        fmt.Println(moves)
+    }
 }
