a92ae0712990f682acb0c3085d442427f8d9f84b
daosyn <daosyn@pm.me>
2018-08-03 00:12:09 -0500

    prevent redundant sequences of moves

--- cuterm.go
+++ cuterm.go
@@ -4,39 +4,56 @@ import "github.com/nsf/termbox-go"
 import "math/rand"
 import "fmt"
 
-var move = [18]string{
-    "F", "F'", "F2",
-    "R", "R'", "R2",
-    "L", "L'", "L2",
-    "U", "U'", "U2",
-    "D", "D'", "D2",
-    "B", "B'", "B2",
-}
+var front = [3]string{"F", "F'", "F2"}
+var back = [3]string{"B", "B'", "B2"}
+var right = [3]string{"R", "R'", "R2"}
+var left = [3]string{"L", "L'", "L2"}
+var up = [3]string{"U", "U'", "U2"}
+var down = [3]string{"D", "D'", "D2"}
 
 func scramble() []string {
-    var s []string
-    for i := 0; i < 30; i ++ {
-        s = append(s, move[rand.Intn(18)])
-        // add logic for redundancies
-    }
-    return s
+	var s []string
+	curr := -1
+	for i := 0; i < 25; i++ {
+		next := rand.Intn(6)
+		if next != curr {
+			switch next {
+			case 0:
+				s = append(s, front[rand.Intn(3)])
+			case 1:
+				s = append(s, back[rand.Intn(3)])
+			case 2:
+				s = append(s, right[rand.Intn(3)])
+			case 3:
+				s = append(s, left[rand.Intn(3)])
+			case 4:
+				s = append(s, up[rand.Intn(3)])
+			case 5:
+				s = append(s, down[rand.Intn(3)])
+			}
+			curr = next
+		} else {
+			i--
+		}
+	}
+	return s
 }
 
 func main() {
-    err := termbox.Init()
-    if err != nil {
-        panic(err)
-    }
-    defer termbox.Close()
-    termbox.SetInputMode(termbox.InputEsc)
-mainloop:
-    for {
-        switch ev := termbox.PollEvent(); ev.Type {
-        case termbox.EventKey:
-            if ev.Key == termbox.KeyEsc {
-                break mainloop
-            }
-        }
-        fmt.Println(scramble())
-    }
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputEsc)
+loop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Key == termbox.KeyEsc {
+				break loop
+			}
+		}
+		fmt.Println(scramble())
+	}
 }
